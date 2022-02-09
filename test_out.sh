1be9df339b94451f5f3cd7f384a65ac15983a85b
agisga <11449372+agisga@users.noreply.github.com>
2022-02-08 23:40:26 -0500

    example for how to execute m2yd model on color MNIST task with manually set numbers for training and test domains

--- test_out.sh
+++ test_out.sh
@@ -5,3 +5,4 @@ python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=20 --aname=deepa
 python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=20 --aname=dann
 python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=2 --aname=hduva
 python main_out.py --te_d=caltech --task=mini_vlcs --debug --bs=20 --aname=matchdg --epochs_ctr=3 --epochs_erm=3   # learning rate is crutial for having NAN
+python main_out.py --te_d 0 1 2 --tr_d 3 4 5 6 7 8 9 --task=mnistcolor --debug --epos=10 --aname=m2yd --zd_dim=7
