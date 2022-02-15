5a83ad70bac0d80082f057da34dcdae4dad2d1fb
Xudong Sun <smilesun.east@gmail.com>
2022-02-15 17:54:08 +0100

    weird, B_net_small works already on Friday, not working now, have fixed the issue

--- libdg/models/bnn/B_net_small.py
+++ libdg/models/bnn/B_net_small.py
@@ -12,12 +12,12 @@ class NetBayes2ConvFC(nn.Module):
     def __init__(self, outputs, i_c, i_h, i_w):
         super().__init__()
         self.conv1 = BBBConv2d(i_c, 32, 5, stride=1, padding=2)
-        self.soft1 = nn.BatchNorm2d(32),
+        self.soft1 = nn.BatchNorm2d(32)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
 
         self.conv2 = BBBConv2d(32, 64, 5, stride=1, padding=2)
-        self.soft2 = nn.BatchNorm2d(32),
+        self.soft2 = nn.BatchNorm2d(64)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
@@ -27,8 +27,9 @@ class NetBayes2ConvFC(nn.Module):
                            self.conv2, self.soft2, self.relu2, self.pool2,
                            self.flatten]
 
-        self.hdim, _ = bn_forward(self.conv_layer, i_c=i_c, i_h=i_h, i_w=i_w)
-        self.fc1 = BBBLinearFactorial(self.hdim, 1000)
+        x,  _ = bn_forward(self.conv_layer, i_c=i_c, i_h=i_h, i_w=i_w)
+        self.h_dim = x.shape[1]
+        self.fc1 = BBBLinearFactorial(self.h_dim, 1000)
         #self.fc1 = BBBLinearFactorial(2 * 2 * 128, 1000)  # only for 32*32 image
         self.soft5 = nn.Softplus()
 
@@ -38,10 +39,18 @@ class NetBayes2ConvFC(nn.Module):
         self.fc3 = BBBLinearFactorial(1000, outputs)
 
         layers = [self.conv1, self.soft1, self.pool1, self.conv2, self.soft2, self.pool2,
-                  self.conv3, self.soft3, self.pool3, self.flatten, self.fc1, self.soft5,
+                  self.flatten, self.fc1, self.soft5,
                   self.fc2, self.soft6, self.fc3]
 
         self.layers = nn.ModuleList(layers)
+
+    def forward(self, x):
+        """forward.
+
+        :param x:
+        """
+        logits = self.probforward(x)
+        return logits
 
     def probforward(self, x):
         'Forward pass with Bayesian weights'
