fb1b0fdfbf479bf0ec6de112649532937bd871b9
Xudong Sun <smilesun.east@gmail.com>
2022-02-15 18:15:27 +0100

    add script for user to benchmark bayes vs frequenst net

---
+++ benchmark_bayes.sh
@@ -0,0 +1,4 @@
+python main_out.py --te_d=0 --aname=deepall --task=mnistcolor4 --mpath=./libdg/models/bnn/B_net_small.py --epos=500
+python main_out.py --te_d=0 --aname=deepall --task=mnistcolor4 --mpath=./libdg/models/bnn/F_net_small.py --epos=500
+python main_out.py --te_d=0 --aname=deepall --task=mnistcolor4 --mpath=./libdg/models/bnn/Bayesian3Conv3FC.py --epos=500
+python main_out.py --te_d=0 --aname=deepall --task=mnistcolor4 --mpath=./libdg/models/bnn/F3Conv3FC.py --epos=500
